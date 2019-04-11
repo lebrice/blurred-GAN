707b556fb56e2a1bf8d31dc66c5922a63f7caa29
Fabrice Normandin <fabrice.normandin@gmail.com>
2019-04-11 01:30:07 -0400

    Trying to link 'effective resolution' to 'std'
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- blurred-gan.py
+++ blurred-gan.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 
 tf.enable_eager_execution()
-from gaussian_blur import gaussian_blur, GaussianBlur2D, appropriate_kernel_size
+from gaussian_blur import gaussian_blur, GaussianBlur2D, appropriate_kernel_size, effective_resolution_reduction
 
 print(tf.__version__)
 from progan import Generator, Discriminator
@@ -23,23 +23,30 @@ df = "NHWC"
 stats = lambda t: (tf.reduce_min(t), tf.reduce_mean(t), tf.reduce_max(t))
 
 
+    
+
 # blurring_layer = GaussianBlur2D(blur_std, data_format=df)
 
 def main():
     with tf.Graph().as_default():
-        _blur_std = tf.Variable(10.0, trainable=False)
+        _blur_std = tf.Variable(30.0, trainable=False)
         
+        res_factor = tf.Variable(0.0, trainable=False)
+
         latents = tf.random_normal([1, 512])
         
         # Generator used for testing.
         generator = hub.Module("https://tfhub.dev/google/progan-128/1")
         # generator = Generator(128)
-        
+                
         _images = generator(latents)
 
-        _kernel_size = appropriate_kernel_size(_blur_std, _images.shape[1].value)
-        with tf.control_dependencies([tf.print("kernel_size:", _kernel_size)]):
-            _blurred_images = gaussian_blur(_images, _blur_std, kernel_size=_kernel_size)
+        # _kernel_size = appropriate_kernel_size(_blur_std, _images.shape[1].value)
+        _blurred_images = effective_resolution_reduction(
+            _images,
+            res_factor,
+        )
+        # _blurred_images = gaussian_blur(_images, _blur_std, kernel_size=_kernel_size)
 
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
@@ -47,24 +54,43 @@ def main():
             # get a latent vector
             latent_vector = sess.run(latents)
                         
-            original = sess.run(_images, feed_dict={latents: latent_vector})
-
-            fig, axeslist = plt.subplots(ncols=3, nrows=2)      
-            for i in range(5):
-                blurred_image, std, k = sess.run([_blurred_images, _blur_std, _kernel_size], feed_dict={
-                    latents: latent_vector,
-                    _kernel_size: 11
+            original = sess.run(_images, feed_dict={
+                latents: latent_vector,
+                # _kernel_size: 3,
+                # _blur_std: 0.1,
+                res_factor: 0.0,
                 })
 
+
+           
+
+            # sess.run(_blur_std.assign(32))
+            # factor = 1.0
+
+
+            fig, axeslist = plt.subplots(ncols=5, nrows=2)
+
+            for i, factor in enumerate(np.arange(1, 0, -0.1)):
+                # blurred_image, std, k = sess.run(
+                blurred_image, res = sess.run(
+                    # [_blurred_images, _blur_std, _kernel_size],
+                    [_blurred_images, res_factor],
+                    feed_dict={
+                        latents: latent_vector,
+                        # _kernel_size: 5,
+                        # _blur_std: 1.0,
+                        res_factor: factor
+                    })
+
                 axeslist.ravel()[i].imshow(blurred_image[0])
-                axeslist.ravel()[i].set_title(f"\u03C3={std}, k={k}")
+                # axeslist.ravel()[i].set_title(f"\u03C3={std}, k={k}")
+                axeslist.ravel()[i].set_title("original" if i == 0 else f"factor: {res:.2%}")
                 axeslist.ravel()[i].set_axis_off()
 
-                sess.run(_blur_std.assign(_blur_std * 0.5))
+                # sess.run(_blur_std.assign(_blur_std * 0.5))
+                # factor /= 2.0
+                # sess.run(res_factor.assign(res_factor * 0.5))
 
-            axeslist.ravel()[-1].imshow(original[0])
-            axeslist.ravel()[-1].set_title("Original")
-            axeslist.ravel()[-1].set_axis_off()
 
             plt.tight_layout() # optional
             plt.show()
--- gaussian_blur.py
+++ gaussian_blur.py
@@ -5,72 +5,110 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 import tensorflow.keras as keras
 import tensorflow.math as math
+from typing import Tuple
+
+def effective_resolution_reduction(images: tf.Tensor, factor: float) -> tf.Tensor:
+    """
+    reduces the "effective resolution" of the image by `factor`, using Gaussian Blur.
+
+    image: tf.Tensor
+    factor: float in [0, 1)
+
+    TODO: validate the math here. I'm making this up based on my intuition at the moment.
+    """
+
+    h, w, c = get_image_dims(images)
+    full_resolution = math.maximum(h, w)
+    effective_resolution = factor * tf.cast(full_resolution, tf.float32)
+    std = - 10 * math.log(factor)
+
+    std = tf.clip_by_value(std, 1e-2, 0.5 * tf.cast(full_resolution, float))
+    kernel_size = appropriate_kernel_size(std, full_resolution)
+    with tf.control_dependencies([tf.print(std, kernel_size)]):
+        return gaussian_blur(images, std, kernel_size)
 
 
-def appropriate_kernel_size(std: float, image_width: int) -> int:
+
+def appropriate_kernel_size(std: float, image_res: int) -> int:
     """
     Returns the appropriate gaussian kernel size to be used for a given standard deviation.
     """
     # no need to reach more than 3 std's to either side of the middle:
-    size = (2 * math.ceil(std * 3 - 1)) + 1
-    size = math.minimum(math.maximum(size, 3), image_width)
+    size = math.ceil(std * 6)
+    size = (size * 2) // 2 + 1.0  # nearest odd number.
+    size = math.minimum(math.maximum(size, 3), tf.cast(image_res, tf.float32))
     print("std: ", std, "size:", size)
     return size
+
+
+def get_data_format(image) -> str:
+    if image.shape[-1].value in (1, 3):
+        return "NHWC"
+    else:
+        return "NCHW"
+
+
+def get_image_dims(image) -> Tuple[int, int]:
+    data_format = get_data_format(image)
+    image_height = image.shape[1 if data_format == "NHWC" else -2]
+    image_width = image.shape[2 if data_format == "NHWC" else -1]
+    image_channels = image.shape[-1 if data_format == "NHWC" else 1]
+    return image_height, image_width, image_channels
 
 
 # @tf.function
 def gaussian_blur(
     image: tf.Tensor,
     std: float,
-    kernel_size: int = None,
-    data_format=None,
-    show_kernel=False,
+    kernel_size: int = None
 ):
-    if data_format is None:
-        if image.shape[-1] in (1, 3):
-            data_format = "NHWC"
-        else:
-            data_format = "NCHW"
-    assert data_format in {"NHWC", "NCHW"}
+    """
+    Performs gaussian blurring.
+
+    NOTE: Since the gaussian filter is separable, we use a 1d kernel and
+    convolve twice (more efficient).
+    """
+    data_format = get_data_format(image)
+    assert data_format in {"NHWC", "NCHW"}, "invalid data format"
     
-    image_width = image.shape[2 if data_format == "NHWC" else -1]
-    image_channels = image.shape[-1 if data_format == "NHWC" else 1]
-    
+    h, w, c = get_image_dims(image)
+
     if kernel_size is None:
         size = appropriate_kernel_size(std, image_width.value)
     else:
         size = kernel_size
 
-    d = tfp.distributions.Normal(0, std)
-    vals = d.prob(tf.range(-(size//2), (size//2)+1, dtype=tf.float32))
-    kernel = tf.einsum('i,j->ij', vals, vals)
+    distribution = tfp.distributions.Normal(0, std)
+    vals = distribution.prob(tf.range(-(size//2), (size//2)+1, dtype=float))
+    kernel = vals / tf.reduce_sum(vals)
     
-    if tf.executing_eagerly() and show_kernel:
-        import matplotlib.pyplot as plt
-        from matplotlib import cm
-        from mpl_toolkits.mplot3d import Axes3D
-        # Make data.
-        X = np.arange(0, size, 1)
-        Y = np.arange(0, size, 1)
-        X, Y = np.meshgrid(X, Y)
-        fig = plt.figure()
-        ax = fig.gca(projection='3d')
-        ax.plot_surface(X, Y, kernel, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-        plt.show()
+    summary = tf.summary.image(
+        "gaussian_kernel",
+        tf.einsum("i,j->ij", kernel, kernel)[tf.newaxis, :, :, tf.newaxis]
+    )
 
-    kernel /= tf.reduce_sum(kernel)  # normalize the kernel
-
-    # expand dims for conv2d operator
-    kernel = kernel[:, :, tf.newaxis, tf.newaxis]
-    kernel = tf.tile(kernel, [1, 1, image_channels, 1])
-
-    return tf.nn.depthwise_conv2d(
+    # expand the kernel to match the requirements of depthsiwe_conv2d
+    kernel = kernel[:, tf.newaxis, tf.newaxis, tf.newaxis]
+    kernel_h = tf.tile(kernel, [1, 1, c, 1])
+    kernel_v = tf.transpose(kernel_h, [1, 0, 2, 3])
+        
+    result_1 = tf.nn.depthwise_conv2d(
         image,
-        kernel,
+        kernel_h,
         strides=[1, 1, 1, 1],
         padding="SAME",
         data_format=data_format,
     )
+    # flip the kernel, so it is now vertical
+    result_2 = tf.nn.depthwise_conv2d(
+        result_1,
+        kernel_v,
+        strides=[1, 1, 1, 1],
+        padding="SAME",
+        data_format=data_format,
+    )
+
+    return result_2
 
 
 class GaussianBlur2D(keras.layers.Layer):
@@ -96,3 +134,18 @@ class GaussianBlur2D(keras.layers.Layer):
             std=self.std,
             #data_format=self.data_format,
         )
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    from matplotlib import cm
+    from mpl_toolkits.mplot3d import Axes3D
+    kernel_2d = tf.einsum('i,j->ij', kernel_1, kernel_2)
+    # Make data.
+    X = np.arange(0, size, 1)
+    Y = np.arange(0, size, 1)
+    X, Y = np.meshgrid(X, Y)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(X, Y, kernel, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    plt.show()
