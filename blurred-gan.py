a6a394d82144a45991b5ad98e655059510a796dc
Fabrice Normandin <fabrice.normandin@gmail.com>
2019-04-30 18:52:29 -0400

    Much nicer code!!
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- blurred-gan.py
+++ blurred-gan.py
@@ -1,100 +1,286 @@
-"""
-Progressively Growing GANS using Gaussian Blur
-"""
 import tensorflow as tf
-import tensorflow_hub as hub
-import tensorflow_probability as tfp
-import tensorflow.keras as keras
-import tensorflow.math as math
-
-import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow_datasets as tfds
+from tensorflow.keras import layers
+from typing import Callable
 
 
-tf.enable_eager_execution()
-from gaussian_blur import gaussian_blur, GaussianBlur2D, appropriate_kernel_size, effective_resolution_reduction
-
-print(tf.__version__)
-from progan import Generator, Discriminator
-
-df = "NHWC"
+from gaussian_blur import GaussianBlur2D, Function
 
 
-stats = lambda t: (tf.reduce_min(t), tf.reduce_mean(t), tf.reduce_max(t))
+class Generator(tf.keras.Sequential):
+    def __init__(self, latent_size=100, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.latent_size = latent_size
+        self.add(layers.Dense(4*4*512, use_bias=False, input_shape=(self.latent_size,)))
+        self.add(layers.BatchNormalization())
+        self.add(layers.LeakyReLU())
+
+        self.add(layers.Reshape((4, 4, 512)))
+        assert self.output_shape == (None, 4, 4, 512) # Note: None is the batch size
+
+        self.add(layers.Conv2DTranspose(512, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+        assert self.output_shape == (None, 4, 4, 512), self.output_shape
+        self.add(layers.BatchNormalization())
+        self.add(layers.LeakyReLU())
+
+        self.add(layers.Conv2DTranspose(256, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+        assert self.output_shape == (None, 8, 8, 256), self.output_shape
+        self.add(layers.BatchNormalization())
+        self.add(layers.LeakyReLU())
+
+        self.add(layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+        assert self.output_shape == (None, 16, 16, 128), self.output_shape
+        self.add(layers.BatchNormalization())
+        self.add(layers.LeakyReLU())
+
+        self.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+        assert self.output_shape == (None, 32, 32, 64), self.output_shape
+        self.add(layers.BatchNormalization())
+        self.add(layers.LeakyReLU())
+
+        self.add(layers.Conv2DTranspose(32, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+        assert self.output_shape == (None, 64, 64, 32), self.output_shape
+        self.add(layers.BatchNormalization())
+        self.add(layers.LeakyReLU())
+
+        self.add(layers.Conv2DTranspose(16, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+        assert self.output_shape == (None, 128, 128, 16), self.output_shape
+        self.add(layers.BatchNormalization())
+        self.add(layers.LeakyReLU())
+
+        self.add(layers.Conv2D(3, (5, 5), padding='same', use_bias=False, activation='tanh'))
+        assert self.output_shape == (None, 128, 128, 3), self.output_shape
 
 
+class Discriminator(tf.keras.Sequential):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add(layers.Conv2D(16, 5, strides=2, padding='same', input_shape=[128, 128, 3]))
+        self.add(layers.LeakyReLU())
+        self.add(layers.Dropout(0.3))
+
+        self.add(layers.Conv2D(32, 5, strides=2, padding='same'))
+        self.add(layers.LeakyReLU())
+        self.add(layers.Dropout(0.3))
+
+        self.add(layers.Conv2D(64, 5, strides=2, padding='same'))
+        self.add(layers.LeakyReLU())
+        self.add(layers.Dropout(0.3))
+
+        self.add(layers.Conv2D(128, 5, strides=2, padding='same'))
+        self.add(layers.LeakyReLU())
+        self.add(layers.Dropout(0.3))
+
+        self.add(layers.Conv2D(256, 5, strides=2, padding='same'))
+        self.add(layers.LeakyReLU())
+        self.add(layers.Dropout(0.3))
+
+        self.add(layers.Conv2D(512, 5, strides=2, padding='same'))
+        self.add(layers.LeakyReLU())
+        self.add(layers.Dropout(0.3))
+
+        self.add(layers.Flatten())
+        self.add(layers.Dense(1))
+
+
+class DiscriminatorWithBlur(tf.keras.Model):
+    def __init__(self, blur_std: float, discriminator_constructor: Callable, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.blur_std = blur_std
+        self.blurring_layer = GaussianBlur2D(self.blur_std)
+        self.disc = discriminator_constructor()
+
+    def call(self, x: tf.Tensor, training=False):
+        self.blurring_layer.std = self.blur_std
+        x = self.blurring_layer(x)
+        x = self.disc(x, training=training)
+        return x
+
+
+class GAN(tf.keras.Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.generator = Generator()
+        self.generator_optimizer = tf.keras.optimizers.Adam()
+
+        self.std = tf.Variable(1.0, name="blur_std", trainable=False)
+        self.discriminator = DiscriminatorWithBlur(self.std, Discriminator)
+        self.discriminator_optimizer = tf.keras.optimizers.Adam()
+
+        self.gp_coefficient = 10.0
+        self.batch_size = None
+
+        self.real_scores = tf.keras.metrics.Mean("real_scores", dtype=tf.float32)
+        self.fake_scores = tf.keras.metrics.Mean("fake_scores", dtype=tf.float32)
+        self.gp_term = tf.keras.metrics.Mean("gp_term", dtype=tf.float32)
+
+
+        self.gen_loss = tf.keras.metrics.Mean("gen_loss", dtype=tf.float32)
+        self.disc_loss = tf.keras.metrics.Mean("disc_loss", dtype=tf.float32)
+
+    def latents_batch(self):
+        assert self.batch_size is not None
+        return tf.random.uniform([self.batch_size, self.generator.latent_size])
+
+    def generate_samples(self, latent_vector=None, training=False):
+        if latent_vector is None:
+            latents = self.latents_batch()
+        return self.generator(latents, training=training)
+
+    def gradient_penalty(self, reals, fakes):
+        # interpolate between real and fakes
+        batch_size = reals.shape[0]
+        a = tf.random.uniform([batch_size, 1, 1, 1])
+        x_hat = reals + a * (fakes - reals)
+
+        with tf.GradientTape() as tape:
+            tape.watch(x_hat)    
+            y_hat = self.discriminator(x_hat, training=True)
+
+        grad = tape.gradient(y_hat, x_hat)
+        norm = tf.norm(tf.reshape(grad, [tf.shape(grad)[0], -1]), axis=1)
+        gp_term = self.gp_coefficient * tf.reduce_mean((norm - 1.)**2)
+        return gp_term
+
+    # @Function
+    def discriminator_step(self, reals):
+        self.batch_size = reals.shape[0]
+        with tf.GradientTape() as disc_tape:
+            fakes = self.generate_samples(training=False)
+            fake_scores = self.discriminator(fakes, training=True)
+            real_scores = self.discriminator(reals, training=True)
+            gp_term = self.gradient_penalty(reals, fakes)
+            disc_loss = tf.reduce_mean(fake_scores - real_scores) + gp_term
+
+        # save metrics.
+        self.fake_scores(fake_scores)
+        self.real_scores(real_scores)
+        self.gp_term(gp_term)
+        self.disc_loss(disc_loss)
+
+        discriminator_grads = disc_tape.gradient(disc_loss, self.discriminator.trainable_variables)
+        self.discriminator_optimizer.apply_gradients(zip(discriminator_grads, self.discriminator.trainable_variables))
+        
+        return disc_loss
     
-
-# blurring_layer = GaussianBlur2D(blur_std, data_format=df)
-
-def main():
-    with tf.Graph().as_default():
-        _blur_std = tf.Variable(30.0, trainable=False)
+    # @Function
+    def generator_step(self):
+        with tf.GradientTape() as gen_tape:
+            fakes = self.generate_samples(training=True)
+            fake_scores = self.discriminator(fakes, training=False)
+            gen_loss = tf.reduce_mean(fake_scores)
         
-        res_factor = tf.Variable(0.0, trainable=False)
+        # save metrics.
+        self.fake_scores(fake_scores)
+        self.gen_loss(gen_loss)
 
-        latents = tf.random_normal([1, 512])
-        
-        # Generator used for testing.
-        generator = hub.Module("https://tfhub.dev/google/progan-128/1")
-        # generator = Generator(128)
-                
-        _images = generator(latents)
-
-        # _kernel_size = appropriate_kernel_size(_blur_std, _images.shape[1].value)
-        _blurred_images = effective_resolution_reduction(
-            _images,
-            res_factor,
-        )
-        # _blurred_images = gaussian_blur(_images, _blur_std, kernel_size=_kernel_size)
-
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-
-            # get a latent vector
-            latent_vector = sess.run(latents)
-                        
-            original = sess.run(_images, feed_dict={
-                latents: latent_vector,
-                # _kernel_size: 3,
-                # _blur_std: 0.1,
-                res_factor: 0.0,
-                })
+        generator_grads = gen_tape.gradient(gen_loss, self.generator.trainable_variables)
+        self.generator_optimizer.apply_gradients(zip(generator_grads, self.generator.trainable_variables))
+        return gen_loss
 
 
-           
+def celeba_dataset(batch_size=32, epochs=None, shuffle_buffer_size=100) -> tf.data.Dataset:
+    """Modern Tensorflow input pipeline for the CelebA dataset"""
 
-            # sess.run(_blur_std.assign(32))
-            # factor = 1.0
+    @Function
+    def take_image(example):
+        return example["image"]
 
+    @Function
+    def normalize(image):
+        return (tf.cast(image, tf.float32) - 127.5) / 127.5
 
-            fig, axeslist = plt.subplots(ncols=5, nrows=2)
+    @Function
+    def resize_image(image):
+        image = tf.image.resize(image, [128, 128])
+        return image
 
-            for i, factor in enumerate(np.arange(1, 0, -0.1)):
-                # blurred_image, std, k = sess.run(
-                blurred_image, res = sess.run(
-                    # [_blurred_images, _blur_std, _kernel_size],
-                    [_blurred_images, res_factor],
-                    feed_dict={
-                        latents: latent_vector,
-                        # _kernel_size: 5,
-                        # _blur_std: 1.0,
-                        res_factor: factor
-                    })
+    @Function
+    def preprocess_images(image):
+        image = normalize(image)
+        image = resize_image(image)
+        return image
 
-                axeslist.ravel()[i].imshow(blurred_image[0])
-                # axeslist.ravel()[i].set_title(f"\u03C3={std}, k={k}")
-                axeslist.ravel()[i].set_title("original" if i == 0 else f"factor: {res:.2%}")
-                axeslist.ravel()[i].set_axis_off()
-
-                # sess.run(_blur_std.assign(_blur_std * 0.5))
-                # factor /= 2.0
-                # sess.run(res_factor.assign(res_factor * 0.5))
-
-
-            plt.tight_layout() # optional
-            plt.show()
+    celeba_dataset = tfds.load(name="celeb_a", split=tfds.Split.ALL)
+    celeba = (celeba_dataset
+              .map(take_image)
+              .batch(16)
+              .map(preprocess_images)
+              .apply(tf.data.experimental.unbatch())
+              .shuffle(shuffle_buffer_size)
+              .batch(batch_size)
+              .prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+              .repeat(epochs)
+              )
+    return celeba
 
 
 if __name__ == "__main__":
-    main()
+    import os
+    import matplotlib.pyplot as plt
+    import datetime
+    gan = GAN()
+
+    EPOCHS = 10
+    batch_size = 16
+    d_steps_per_g_step = 3
+    save_interval = 50
+
+    # interval at which tensorboard summaries should be saved
+    tick_interval = 100
+
+    dataset = celeba_dataset(batch_size=batch_size)
+
+    results_dir = "./results"
+    checkpoint_dir = results_dir + "/testrun"
+
+    # number of batches. TODO: change to number of examples seen. 
+    step = tf.Variable(1)
+
+    eval_latents = tf.random.uniform([8, gan.generator.latent_size], seed=123)
+
+    # checkpoint setup
+    checkpoint = tf.train.Checkpoint(step=step, gan=gan)
+    manager = tf.train.CheckpointManager(checkpoint, checkpoint_dir, max_to_keep=3)
+    checkpoint.restore(manager.latest_checkpoint)
+
+    # tensorboard setup
+    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    train_log_dir = checkpoint_dir + '/logs/gradient_tape/' + current_time + '/train'
+    train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+
+    if manager.latest_checkpoint:
+        print(f"Restored from {manager.latest_checkpoint}")
+    else:
+        print("Initializing from scratch.")
+
+    print(f"Starting at step: {int(step.numpy())}")
+    for image_batch in dataset:
+        _step = int(step.numpy())
+
+        message = f"step: {_step}, std: {gan.std.numpy()}"
+        # Train the discriminator
+        d_loss = gan.discriminator_step(image_batch)
+
+        message += f"\t d loss: {d_loss.numpy()}"
+        if _step % d_steps_per_g_step == 0:
+            # Train the Generator
+            g_loss = gan.generator_step()
+            message += f"\t G loss: {g_loss.numpy()}"
+        
+        print(message)
+
+        if _step % save_interval == 0:
+            save_path = manager.save()
+            print(f"Saved checkpoint for step {step}: {save_path}")
+
+        if _step % tick_interval == 0:
+            with train_summary_writer.as_default():
+                for metric in gan.metrics:
+                    tf.summary.scalar(metric.name, metric.result(), step=_step)
+                fakes = gan.generate_samples(eval_latents)
+                tf.summary.image("fakes", fakes)
+        
+        # increment the step.
+        step.assign_add(1)
--- gaussian_blur.py
+++ gaussian_blur.py
@@ -2,92 +2,114 @@
 2D Gaussian Blur Keras layer.
 """
 import tensorflow as tf
-import tensorflow_probability as tfp
 import tensorflow.keras as keras
 import tensorflow.math as math
-from typing import Tuple
+from math import pi
 
-def effective_resolution_reduction(images: tf.Tensor, factor: float) -> tf.Tensor:
-    """
-    reduces the "effective resolution" of the image by `factor`, using Gaussian Blur.
-
-    image: tf.Tensor
-    factor: float in [0, 1)
-
-    TODO: validate the math here. I'm making this up based on my intuition at the moment.
-    """
-
-    h, w, c = get_image_dims(images)
-    full_resolution = math.maximum(h, w)
-    effective_resolution = factor * tf.cast(full_resolution, tf.float32)
-    std = - 10 * math.log(factor)
-
-    std = tf.clip_by_value(std, 1e-2, 0.5 * tf.cast(full_resolution, float))
-    kernel_size = appropriate_kernel_size(std, full_resolution)
-    with tf.control_dependencies([tf.print(std, kernel_size)]):
-        return gaussian_blur(images, std, kernel_size)
+print(tf.__version__)
+if tf.__version__[0] == '2':
+    Function = tf.function
+else:
+    Function = tf.contrib.eager.defun
 
 
+def maximum_reasonable_std(image_resolution: int) -> float:
+    kernel_size = image_resolution - 1
+    std = appropriate_std(kernel_size)
+    return std
 
-def appropriate_kernel_size(std: float, image_res: int) -> int:
+
+def appropriate_kernel_size(std: float) -> int:
     """
     Returns the appropriate gaussian kernel size to be used for a given standard deviation.
     """
-    # no need to reach more than 3 std's to either side of the middle:
-    size = math.ceil(std * 6)
-    size = (size * 2) // 2 + 1.0  # nearest odd number.
-    size = math.minimum(math.maximum(size, 3), tf.cast(image_res, tf.float32))
-    print("std: ", std, "size:", size)
-    return size
+    # nearest odd number to 6*std.
+    return (6 * std) * 2 // 2 + 1
+
+
+def appropriate_std(kernel_size):
+    std = (kernel_size-1.0) / 6.0
+    return std
 
 
 def get_data_format(image) -> str:
-    if image.shape[-1].value in (1, 3):
+    last_dim = image.shape[-1]
+    if last_dim in (1, 3):
         return "NHWC"
     else:
         return "NCHW"
 
 
-def get_image_dims(image) -> Tuple[int, int]:
+def get_image_dims(image):
     data_format = get_data_format(image)
-    image_height = image.shape[1 if data_format == "NHWC" else -2]
+    image_height = image.shape[1 if data_format == "NHWC" else 2]
     image_width = image.shape[2 if data_format == "NHWC" else -1]
     image_channels = image.shape[-1 if data_format == "NHWC" else 1]
     return image_height, image_width, image_channels
 
 
-# @tf.function
+def blur_images(images: tf.Tensor, scale: float) -> tf.Tensor:
+    """
+    Performs gaussian blurring. If not given, the right kernel size is infered for the given std.
+    The scale corresponds to the desired standard deviation of the gaussian blurring used.
+    """
+
+    # add the blurring:
+
+    h, w, c = get_image_dims(images)
+    full_resolution = tf.cast(tf.math.maximum(h, w), tf.float32)
+
+    # Ensure maximum element of x is smaller or equal to 1
+    # std = math.sqrt(scale)
+    std = scale
+
+    kernel_size = appropriate_kernel_size(std)
+    # we won't use a kernel bigger than the resolution of the image!
+    kernel_size = tf.clip_by_value(kernel_size, 3, full_resolution)
+    
+    # In case the kernel size was clipped, we make sure to get the right std for that kernel size.
+    # If we don't do this, we might end up with a huge kernel, but with high values even at the edges.
+    std = appropriate_std(kernel_size)
+    std = tf.math.maximum(std, 0.01)
+    #with tf.device("cpu:0"), tf.variable_scope("gaussian_blur", reuse=tf.AUTO_REUSE):
+    #    tf.summary.scalar("kernel_size", kernel_size)
+    #    tf.summary.scalar("std", std)
+    #    tf.summary.scalar("scale", scale)
+
+    # Warn the user if the scale given is larger than what is reasonable.
+    # with tf.control_dependencies([tf.print("scale:", scale, "std:", std, "kernel_size:", kernel_size)]):
+    return gaussian_blur(images, std, kernel_size)
+
+
+def gaussian_kernel_1d(std, kernel_size):
+    x = tf.range(-(kernel_size//2), (kernel_size//2)+1, dtype=float)
+    g = tf.exp(- (x**2 / (2 * std**2))) / (tf.sqrt(2 * pi) * std)
+    # normalize the sum to 1
+    g = g / tf.reduce_sum(g)
+    return g
+
+
+@Function
 def gaussian_blur(
-    image: tf.Tensor,
+    image,
     std: float,
-    kernel_size: int = None
+    kernel_size: int,
 ):
     """
-    Performs gaussian blurring.
-
+    Performs gaussian blurring with a gaussian kernel of standard variation `std` and size `kernel_size`.
     NOTE: Since the gaussian filter is separable, we use a 1d kernel and
     convolve twice (more efficient).
+
+    Use 'blur_images' for additional validation of the std and kernel_size, potentially saving on performance.
     """
     data_format = get_data_format(image)
     assert data_format in {"NHWC", "NCHW"}, "invalid data format"
     
-    h, w, c = get_image_dims(image)
-
-    if kernel_size is None:
-        size = appropriate_kernel_size(std, image_width.value)
-    else:
-        size = kernel_size
-
-    distribution = tfp.distributions.Normal(0, std)
-    vals = distribution.prob(tf.range(-(size//2), (size//2)+1, dtype=float))
-    kernel = vals / tf.reduce_sum(vals)
-    
-    summary = tf.summary.image(
-        "gaussian_kernel",
-        tf.einsum("i,j->ij", kernel, kernel)[tf.newaxis, :, :, tf.newaxis]
-    )
+    kernel = gaussian_kernel_1d(std, kernel_size)
+    kernel = tf.identity(kernel, name="gaussian_kernel")
 
     # expand the kernel to match the requirements of depthsiwe_conv2d
+    h, w, c = get_image_dims(image)
     kernel = kernel[:, tf.newaxis, tf.newaxis, tf.newaxis]
     kernel_h = tf.tile(kernel, [1, 1, c, 1])
     kernel_v = tf.transpose(kernel_h, [1, 0, 2, 3])
@@ -112,40 +134,10 @@ def gaussian_blur(
 
 
 class GaussianBlur2D(keras.layers.Layer):
-    def __init__(
-        self,
-        std: float,
-        data_format="channels_first",
-        *args,
-        **kwargs
-    ):
+    def __init__(self, std: float, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.std = std
-        self.data_format = data_format
-        
         self.trainable = False
-        
-#     @tf.function
+
     def call(self, image: tf.Tensor):
-        flipped = tf.transpose(image, [0,2,3,1])
-        tf.summary.image("image", flipped)
-        return gaussian_blur(
-            image,
-            std=self.std,
-            #data_format=self.data_format,
-        )
-
-
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    from matplotlib import cm
-    from mpl_toolkits.mplot3d import Axes3D
-    kernel_2d = tf.einsum('i,j->ij', kernel_1, kernel_2)
-    # Make data.
-    X = np.arange(0, size, 1)
-    Y = np.arange(0, size, 1)
-    X, Y = np.meshgrid(X, Y)
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.plot_surface(X, Y, kernel, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-    plt.show()
+        return blur_images(image, self.std)
